41c3667d2a17d1930623bd17c84cf397de9e6832
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-19 03:15:48 -0300

    take a break

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const routes = require(".");
+
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
 
 const port = 3000;
 
