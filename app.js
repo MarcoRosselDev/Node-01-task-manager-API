f358f17a7aa07d27f6cb1b18e7dc8c9d6a7d37ab
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-19 21:42:31 -0300

    router

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const routes = require(".");
+const routes = require("./routes/tasks");
 
 app.get("/hello", (req, res) => {
   res.send("Task Manager App");
