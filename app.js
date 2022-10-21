38147a604123118f8648845b334754e5fde3d758
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-20 23:16:20 -0300

    saved the url on .env

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
+require("dotenv").config();
 
 app.get("/hello", (req, res) => {
   res.send("Task Manager App");
@@ -21,7 +22,7 @@ const port = 3000;
 
 const start = async () => {
   try {
-    await connectDB();
+    await connectDB(process.env.MONGO_URL);
     app.listen(port, console.log(`server is listening on port ${port}...`));
   } catch (error) {
     console.log(error);
