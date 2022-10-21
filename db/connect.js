d8044f5b593f7a9a5a10bd2e70ff2df2b09e70e3
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-20 22:31:42 -0300

    2

--- db/connect.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-
-const connerctionString =
-  "mongodb+srv://marco:<password>@nodeexpressproject.ioc92yd.mongodb.net/NODE-01-TASK-MANAGER?retryWrites=true&w=majority";
-
-mongoose.connect(connerctionString);
