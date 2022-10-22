8c0be8f07deecbf033f8ab73e775ecf1a5044223
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-22 00:07:40 -0300

    getTask if

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -23,6 +23,11 @@ const getTask = async (req, res, next) => {
   try {
     const { id: taskID } = req.params;
     const task = await Task.findOne({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    }
+
     res.status(200).json({ task });
   } catch (error) {}
 };
