4351eb9f8a74cb95039c54fb5914dd25e67628b1
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-21 23:57:11 -0300

    validation getAllTasks

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,11 @@
 const Task = require("../models/Task");
 const getAllTasks = async (req, res) => {
-  const tasks = await Task.find({});
-  res.status(200).json({ tasks });
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const createTask = async (req, res) => {
